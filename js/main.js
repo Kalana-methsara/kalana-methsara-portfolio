e94f3dfba401f525cde77ec2b37ea9d11a772dfb
Kalana Methsara <kalanamethsara53@gmail.com>
2025-10-07 23:21:21 +0530

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Web Designer", "Front-End Developer", "UI/UX Enthusiast"],
+    strings: ["Web Designer", "Full-Stack Developer", "UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
